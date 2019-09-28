5a1cb9786933cf9b4cc942575d7cde2dc2f5fbdc
Alexander Lehman <alexander.bmw325is@gmail.com>
2019-09-27 23:58:01 -0400

    some changes to kinda make it work

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,4 @@
-const wordlist = ["now", "find", "long", "down", "day", "did", "get", "come", "made", "may",
-"part", "over", "yellow", "orange", "brown"];
+const wordlist = ["did","yellow","none","hangman"];
 
 const word = wordlist[Math.floor(Math.random() * wordlist.length)];
 let answerArray = [];
@@ -8,23 +7,64 @@ let answerArray = [];
     }
  let remainingLetters = word.length;
  let guessed = [];
- let guessesLeft = 0;
-
-//document.getElementById("word").innerHTML = answerArray.join(" ");
+ let guessesLeft = word.length + 2;
+function gamesetup() {
+    document.getElementById("word").innerHTML = answerArray.join(" ");
+    document.getElementById("guessesleft").innerHTML = guessesLeft;
+}
 
 document.addEventListener("keydown", whatLetter);
 console.log(word);
 
 function whatLetter(event) {
-
+	
+let foundindex = [];
+	
     let guess = event.key.toLowerCase();
-        console.log(guess);
+    
+    
+        //console.log(guess);
+        //console.log(event.keyCode);
+        if(event.keyCode < 65 || event.keyCode >90) {
+            console.log("Please press a letter key");
+        }
+        //console.log(guess);
+
         // this allows the user to input a letter
     
-            guessed.push(guess);
+            
             //document.getElementById("guessed").innerHTML = guessed;
             //console.log(guessed);
-    
+            //console.log(guessed.indexOf(guess));
+            
+            if(guessed.indexOf(guess) > -1 && guessed.length>0) {
+				console.log("already guessed");
+			} else {
+				guessed.push(guess);
+				
+				if(word.indexOf(guess) === -1) {
+					console.log("wrong" + " " + guess);
+					guessesLeft--;				
+				}
+				for (let j =0; j<word.length; j++) {
+					if(word[j] === guess) {
+						foundindex.push(j);
+					}
+				}
+				for (let j=0;j<foundindex.length;j++) {
+					answerArray[foundindex[j]] = guess;
+				}
+			}           
+            			
+			//console.log("foundindex" + " " + foundindex);			
+			//console.log("answerarray" + " " + answerArray);
+			
+			document.getElementById("guessesleft").innerHTML = guessesLeft;
+			document.getElementById("guessed").innerHTML = guessed;
+			
+			//for(let j=0;j<
+            
+            /*
             //update game state with guessed letter
             for (let j = 0; j <word.length; j++){
                 //for letter in the word
@@ -47,6 +87,7 @@ function whatLetter(event) {
                 )
                    
                 }
+            */
             document.getElementById("word").innerHTML = answerArray.join(" ");
     }
 
